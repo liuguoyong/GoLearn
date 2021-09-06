3b4ec8bbbb9489cab51752dbb1e14ad5e7ad8449
liuguoyong <860279144@qq.com>
2021-09-06 20:27:33 +0800

    第一天学习

--- src/go_code/chapter02/main/Homework.go
+++ src/go_code/chapter02/main/Homework.go
@@ -11,4 +11,6 @@ func main() {
 	fmt.Println("dgbaihdihwdlhseljkhsdjklfhsdjklhfjalsdhfjashfjda",
 		"shjfhasjkhfaklshdfalskhfiuwehfiuwhgferiohguerghuieoshgupeshgue",
 		"shrgpehsrguherspghesrphgusehrg")
+	println(1323)
+
 }
